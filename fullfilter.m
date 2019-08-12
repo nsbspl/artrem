c98bf9a7f5df2a9bdf505e7d5400d90bbbb6533d
Taha Morshedzadeh <taha.morshedzadeh@gmail.com>
2019-08-12 16:53:55 -0400

    update filter files

--- fullfilter.m
+++ fullfilter.m
@@ -1,6 +1,12 @@
 %% add paths
-addpath (genpath ('C:\Users\taham\OneDrive - UHN\DBS_PD'))
-addpath(genpath('C:\Users\taham\Dropbox'))
+
+% the first two, for taha's laptop
+%addpath (genpath ('C:\Users\taham\OneDrive - UHN\DBS_PD'))
+%addpath(genpath('C:\Users\taham\Dropbox'))
+
+%this one for idir's PC
+addpath (C:\Users\User\Desktop\Taha\EEG_data)
+%
 
 %% Import the data from central database in specific accessible variables for this code
 load alldata % load the organized data recorded from patients and uploaded to the UHN OneDrive Cloud
