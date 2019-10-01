4cf685c34bebec1a974fefc82affc16435f933b3
Taha Morshedzadeh <Taha.Morshedzadeh@uhnresearch.ca>
2019-10-01 17:21:58 -0400

    rollback hellos

--- fullfilter.m
+++ fullfilter.m
@@ -1,10 +1,7 @@
 %% add paths
-% helllllo
 % the first two, for taha's laptop
 addpath (genpath ('C:\Users\taham\OneDrive - UHN\DBS_PD'))
 %addpath(genpath('C:\Users\taham\Dropbox'))
-% hello
-
 
 %this one for idir's PC
 %addpath(genpath('C:\Users\User\Desktop\Taha\EEG_data'))
